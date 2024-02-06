c62f114ce383950dee448dcc20dc4d6383404f6e
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-02-06 16:54:30 -0300

    instale iconos de react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css"
 import Menu from './components/common/Menu'
 import Inicio from './components/pages/Inicio'
 import Footer from './components/common/Footer'
