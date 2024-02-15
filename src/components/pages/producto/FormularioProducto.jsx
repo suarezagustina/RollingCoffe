6d7522eec6c9c6caf3a71e978ecc42b3318c003e
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-02-15 18:11:10 -0300

    agregamos sweet alert para mostrar si se pudo crear o no un producto

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,6 +1,7 @@
 import { Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { crearProductoAPI } from "../../../helpers/queries";
+import Swal from "sweetalert2";
 const FormularioProducto = () => {
   const {
     register,
@@ -13,10 +14,19 @@ const FormularioProducto = () => {
     const respuesta = await crearProductoAPI(producto);
     if(respuesta.status === 201){
       //mensaje
+      Swal.fire({
+        title: "Producto creado!",
+        text:  `El producto: ${producto.nombreProducto} fue creado correctamente `,
+        icon: "success"
+      });
       console.log("producto creado")
     reset();
     }else{
-      console.log("ocurrio un error")
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: "Intente crear este producto en unos minutos",
+        icon: "error"
+      });
     }
   };
 
