7b9531f139e123086ee07d78a45a14e1e035dcf7
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-02-22 17:25:03 -0300

    mostramos el producto correspondiente en el componente de detalle de producto

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -34,19 +34,18 @@ const cargarDetalle = async()=>{
             <section className="row mb-5">
             <h1 className="display-4 text-center mb-3">Detalle del producto</h1>
             <article className="col-12 d-flex">
-                <img className="col-6" src="https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/09/14170007/chocotorta-destacada.jpg" alt="" />
+                <img className="col-6" src={producto.imagen} alt="{producto.imagen}" />
             <Card>
       <Card.Body className="col-12">
-        <Card.Title>Chocotorta</Card.Title>
+        <Card.Title>{producto.nombreDelProducto}</Card.Title>
         <Card.Text className="fs-5">
             <hr/>
-            ¡Deliciosa chocotorta de chocolate muy
-            cremosa, con dulce de leche y mucho chocolate!
+            {producto.descripcionBreve}
             <div className="d-flex mb-0">
             <p className="text-success mt-0 mb-0">Categoria: </p>
-            <p className="ms-1 mt-0 mb-0">Dulce</p> 
+            <p className="ms-1 mt-0 mb-0">{producto.categoria}</p> 
             </div>
-            <p className="text-success mt-0 mb-0">Precio: $2.500</p>
+            <p className="text-success mt-0 mb-0">Precio:${producto.precio}</p>
         </Card.Text>
       </Card.Body>
     </Card>
